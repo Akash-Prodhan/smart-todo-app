5ccf08788cbccf6e78156041e99423c5cd260375
Akash Prodhan <akashprodhan78@gmail.com>
2025-08-04 19:06:50 +0530

    add failed messsage

--- src/components/FailedSms.jsx
+++ src/components/FailedSms.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export const FailedSms = () => {
   return (
     <>
-    <div className="bg-blue-600 dark:text-white">
-        Failed Please try again!
+    <div className="dark:text-white flex flex-col justify-center items-center gap-4 py-6">
+        <p className='text-6xl'>😞</p>
+        <p className='text-center'>Oops, Something went wrong. Please try again.</p>
     </div>
     </>
   )
